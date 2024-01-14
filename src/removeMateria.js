fbd157aadffa981373e8bbb1da733aee22ecc74e
Raoni Silva <raoni.silvestre@hotmail.com>
2024-01-14 16:38:23 -0300

    Add removeMateria.js file with functions to remove a subject and subtract total hours

---
+++ src/removeMateria.js
@@ -0,0 +1,43 @@
+function removerMateria(e) {
+  const target = e.target
+
+  const qtdHoras = target.innerHTML.split('(')[1].split('h')[0]
+
+  const horario = target.innerHTML
+    .split('(')[2]
+    .substring(0, target.innerHTML.split('(')[2].length - 1)
+
+  const horario_periodos = horario.substring(horario.length - 3)
+
+  const horario_dias = horario.substring(0, horario.length - 3).split('')
+
+  subtractTotalHours(qtdHoras)
+
+  for (i in horario_dias) {
+    const elemento = document.getElementById(horario_dias[i] + horario_periodos)
+    elemento.style.backgroundColor = 'initial'
+    elemento.innerHTML = elemento.id
+  }
+
+  target.remove()
+
+  const saved = []
+
+  const legenda = document.getElementById('legenda')
+
+  for (item of legenda.children) {
+    saved.push(item.innerHTML)
+  }
+
+  localStorage.setItem('saved', saved)
+}
+
+function subtractTotalHours(hour) {
+  let totalHoras = Number(document.querySelector('#totalHoras').textContent)
+  totalHoras = totalHoras - hour
+  if (totalHoras == 0) {
+    document.querySelector('#totalHoras').innerHTML = ''
+  } else {
+    document.querySelector('#totalHoras').innerHTML = totalHoras
+  }
+}
