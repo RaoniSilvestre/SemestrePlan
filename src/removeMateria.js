7a72404cddd5095ba5fad52c1c32b5920078e0e7
Thiago F. <150091259+thigoai@users.noreply.github.com>
2025-01-24 20:17:07 -0300

    Refatoração do removeMateria.js e popular.js (#2)
    * primeiro
    
    * formatação de nomes de variaveis e funções
    
    * dividir responsabilidades da função removeMateria com obterDadosDoHorario
    
    * dividir responsabilidades da função removeMateria com obterDadosDoHorario
    
    * dividir responsabilidades da função removeMateria com obterDadosDoHorario
    
    * dividindo a função popular_tabela
    
    * mensagem de erro

--- src/removeMateria.js
+++ src/removeMateria.js
@@ -1,46 +1,61 @@
 function removerMateria(e) {
-  const target = e.target
+  const horarioSelecionado = e.target
 
-  const qtdHoras = target.innerHTML.split('(')[1].split('h')[0]
+  const dadosDoHoraio = obterDadosDoHorario(horarioSelecionado)
 
-  const horario = target.innerHTML
-    .split('(')[2]
-    .substring(0, target.innerHTML.split('(')[2].length - 1)
+  const { quantidadeDeHoras, diasDoHorario, periodosDoHorario } = dadosDoHoraio
+  
+  diminuirTotalDeHoras(quantidadeDeHoras)
 
-  const horario_periodos = horario.substring(horario.length - 3)
-
-  const horario_dias = horario.substring(0, horario.length - 3).split('')
-
-  subtractTotalHours(qtdHoras)
-
-  for (i in horario_dias) {
-    const elemento = document.getElementById(horario_dias[i] + horario_periodos)
-    elemento.style.backgroundColor = 'initial'
-    elemento.innerHTML = elemento.id
+  for (const dia of diasDoHorario) {
+    const elemento = document.getElementById(dia + periodosDoHorario);
+    if (elemento) {
+      elemento.style.backgroundColor = 'initial';
+      elemento.innerHTML = elemento.id;
+    }
   }
 
-  target.remove()
+  horarioSelecionado.remove()
 
-  const saved = []
+  const materiasSalvas = []
 
-  const legenda = document.getElementById('legenda')
+  const listaDeMaterias = document.getElementById('legenda')
 
-  for (item of legenda.children) {
-    saved.push(item.innerHTML)
+  for (item of listaDeMaterias.children) {
+    materiasSalvas.push(item.innerHTML)
   }
 
   
-  localStorage.setItem('saved', saved)
+  localStorage.setItem('saved', materiasSalvas)
 }
 
-function subtractTotalHours(hour) {
-  let totalHoras = localStorage.getItem('totalHoras')
-  totalHoras = totalHoras - hour
-  if (totalHoras <= 0) {
+function obterDadosDoHorario(horarioSelecionado) {
+  const quantidadeDeHoras = parseInt(
+    horarioSelecionado.innerHTML.split('(')[1].split('h')[0],
+    10
+  );
+
+  const horarioDeAulas = horarioSelecionado.innerHTML
+    .split('(')[2]
+    .substring(0, horarioSelecionado.innerHTML.split('(')[2].length - 1);
+
+  const periodosDoHorario = horarioDeAulas.substring(horarioDeAulas.length - 3);
+
+  const diasDoHorario = horarioDeAulas.substring(0, horarioDeAulas.length - 3).split('');
+
+  return { quantidadeDeHoras, diasDoHorario, periodosDoHorario };
+}
+
+function diminuirTotalDeHoras(quantidadeDeHorasRemovidas) {
+  let totalDeHoras = localStorage.getItem('totalHoras')
+  
+  totalDeHoras = totalDeHoras - quantidadeDeHorasRemovidas
+
+  if (totalDeHoras <= 0) {
     document.querySelector('#totalHoras').innerHTML = 'Total de horas: 0h'
     localStorage.setItem('totalHoras', 0)
   } else {
-    document.querySelector('#totalHoras').innerHTML ='Total de horas: ' + totalHoras + 'h'
-    localStorage.setItem('totalHoras', totalHoras)
+    document.querySelector('#totalHoras').innerHTML ='Total de horas: ' + totalDeHoras + 'h'
+    localStorage.setItem('totalHoras', totalDeHoras)
   }
 }
